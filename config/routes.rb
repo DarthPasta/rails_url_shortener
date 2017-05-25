6f86fae3be096a281eb373975dadd620a1e52366
darthpasta <liowj.nick@gmail.com>
2017-05-25 17:03:14 +0800

    Short link can be redirected to long url

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 	root 'urls#index'
+	get '/urls/:id/short' => 'urls#short'
 	resources :urls
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
